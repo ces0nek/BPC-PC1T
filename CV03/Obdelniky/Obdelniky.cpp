3a67a86a11e985c413006b31601aecd0e9702b70
J. Knobloch <knobloch@vutbr.cz>
2024-10-03 13:25:28 +0200

    Add CMakeWorkspaceSettings into .gitignore

---
+++ CV03/Obdelniky/Obdelniky.cpp
@@ -0,0 +1,12 @@
+﻿// Obdelniky.cpp : Defines the entry point for the application.
+//
+
+#include "Obdelniky.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello Obdelniky." << endl;
+	return 0;
+}
