cbd434e59c651275f56e849a4158956f00d82988
J. Knobloch <knobloch@vutbr.cz>
2024-10-14 12:58:32 +0200

    Added project CV04 - PrestupnyRok

---
+++ CV04/PrestupnyRok/PrestupnyRok.cpp
@@ -0,0 +1,12 @@
+﻿// PrestupnyRok.cpp : Defines the entry point for the application.
+//
+
+#include "PrestupnyRok.h"
+
+using namespace std;
+
+int main()
+{
+	printf("Ahoj!\nToto je zarodek projektu: PrestupnyRok\n");
+	return 0;
+}
