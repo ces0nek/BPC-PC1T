9bec282dfa0d9a24c1ce5b94b357430404371de7
J. Knobloch <knobloch@vutbr.cz>
2024-10-14 12:54:22 +0200

    Added new project CV05 and also added line into CMakeWorkspaceSettings.

---
+++ CV05/Vector/Vector.cpp
@@ -0,0 +1,19 @@
+﻿// Vector.cpp : Defines the entry point for the application.
+//
+
+#include "Vector.h"
+
+using namespace std;
+
+/*
+* 1. bod: Vytvorte funkce pro soucet, velikost a skalarniSoucin dvou 3-rozmernych vektoru
+* 2. bod: Doplnte funkci pro vektororovySoucin dvou 3-rozmernych vektoru, ktera bude v separatnim souboru. (jiny soubor, nez, kde se nachazi funkce main)
+* 
+* Vsechny vytvorene funkce mohou byt v jednom souboru vectorMath3.c, jejich hlavicky pak v .h, ktery bude prenositelny do jineho projektu. 
+*/
+
+int main()
+{
+	printf("Ahoj!\nZde patri dotaz na zadani vektoru a nasledne zobrazeni vysledku.\nPokud tady je pouze tento text, patri mi 0 bodu. :-)\n");
+	return 0;
+}
